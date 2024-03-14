356f30c9521ca3c4cb34605f73a5b893b5de3933
Gaspare <Gas9220@users.noreply.github.com>
2024-03-14 15:59:41 +0100

    feat: create GFAlertVC

---
+++ GHFollowers/GFAlertVC.swift
@@ -0,0 +1,35 @@
+//
+//  GFAlertVC.swift
+//  GHFollowers
+//
+//  Created by Gaspare Monte on 14/03/24.
+//
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+    let containerView = UIView()
+    let titleLabel = GFTitleLabel(textAlign: .center, fontSize: 20)
+    let messageLabel = GFBodyLabel(textAlign: .center)
+    let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
+
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+
+    init(alertTitle: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = alertTitle
+        self.message = message
+        self.buttonTitle = buttonTitle
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+    }
+}
