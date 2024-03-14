babe0f4f863623bf05d3c97a7ffc7c20c8ed1827
Gaspare <Gas9220@users.noreply.github.com>
2024-03-14 16:10:11 +0100

    feat: configure alert title label

--- GHFollowers/GFAlertVC.swift
+++ GHFollowers/GFAlertVC.swift
@@ -17,6 +17,8 @@ class GFAlertVC: UIViewController {
     var message: String?
     var buttonTitle: String?
 
+    let padding: CGFloat = 20
+
     init(alertTitle: String, message: String, buttonTitle: String) {
         super.init(nibName: nil, bundle: nil)
         self.alertTitle = alertTitle
@@ -32,6 +34,7 @@ class GFAlertVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
         configureContainerView()
+        configureTitleLabel()
     }
 
     func configureContainerView() {
@@ -47,6 +50,18 @@ class GFAlertVC: UIViewController {
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             containerView.widthAnchor.constraint(equalToConstant: 280),
             containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+
+    func configureTitleLabel() {
+        containerView.addSubview(titleLabel)
+        titleLabel.text = alertTitle ?? "Something went wrong"
+
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: padding),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            titleLabel.heightAnchor.constraint(equalToConstant: 28)
         ])
     }
 }
