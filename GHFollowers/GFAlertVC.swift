f247ee7de7da3a2fae3456c4ee98cfbaef083e69
Gaspare <Gas9220@users.noreply.github.com>
2024-03-14 16:03:01 +0100

    feat: configure alert container view

--- GHFollowers/GFAlertVC.swift
+++ GHFollowers/GFAlertVC.swift
@@ -32,4 +32,13 @@ class GFAlertVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
     }
+
+    func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+    }
 }
