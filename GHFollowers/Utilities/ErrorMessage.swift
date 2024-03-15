eb72fd7dbc3be39307ea6150460016304a413c46
Gaspare <Gas9220@users.noreply.github.com>
2024-03-15 11:35:19 +0100

    feat: create ErrorMessage enum

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Gaspare Monte on 15/03/24.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Plepase try again"
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data receiver from the server is invalid. Please try again."
+}
